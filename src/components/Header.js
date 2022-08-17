8b83ad51a2a122fc2842f25fd06fa0e6b75652e8
shade <oladeleshadey@gmail.com>
2022-08-17 05:19:48 -0700

    navigation bar in progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,39 @@
-import React, { useEffect, useState } from 'react';
-
-// import components
+import React, {useState, useEffect} from 'react'
 import Logo from '../assets/img/logo.svg';
 import Nav from '../components/Nav';
 import NavMobile from '../components/NavMobile';
-import Socials from './Socials';
+import Socials from '../components/Socials';
+
 
 const Header = () => {
-  const [bg, setBg] = useState(false);
-
-  useEffect(() => {
+  const [bg, setBg] = useState(false)
+  useEffect(() =>{
     window.addEventListener('scroll', () => {
-      return window.scrollY > 50 ? setBg(true) : setBg(false);
-    });
-  });
-
+      return window.scrollY > 50 ? setBg(true) : setBg(false)
+    })
+  })
   return (
-    <header
-      className={`${
-        bg ? 'bg-tertiary h-20' : 'h-24'
-      } flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300`}
-    >
-      <div className='container mx-auto h-full flex items-center justify-between'>
-        {/* logo */}
-        <a href='#'>
-          <img src={Logo} alt='' />
-        </a>
-        {/* nav */}
-        <div className='hidden lg:block'>
-          <Nav />
-        </div>
-        {/* Socials */}
-        <div className='hidden lg:block'>
-          <Socials />
-        </div>
-        {/* nav mobile*/}
-        <div className='lg:hidden'>
-          <NavMobile />
-        </div>
+    <header 
+    className={`${bg ? 'bg-tetiary h-20' : 'h-24'} 
+    flex items-center fixed top-0 w-full 
+    text-white z-10 transition-all duration-300 `}>
+      
+      <div className='container mx-auto h-full items-center justify-between flex'>
+      <a href='#'>
+        <img src={Logo} alt="" />
+      </a>
+      <div className='hidden lg:block'>
+        <Nav /> 
+      </div>
+      <div className='hidden lg:block'>
+        <Socials />
+      </div>
+      <div className=' lg-hidden'>
+        <NavMobile />
+      </div>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,38 +1,13 @@
-import React from 'react';
-
-// import woman image
-import WomanImg from '../assets/img/banner-woman2.webp';
+import React from 'react'
 
 const Hero = () => {
   return (
-    <section
-      id='home'
-      className='lg:h-[85vh] flex items-center bg-primary lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden'
-    >
-      <div className='container mx-auto h-full'>
-        <div className='flex items-center h-full pt-8'>
-          <div className='flex-1 flex flex-col items-center lg:items-start'>
-            <p className='text-lg text-accent text-md mb-[22px]'>
-              Hey, I'm Jane! 👋
-            </p>
-            <h1 className='text-4xl leading-[44px] md:text-5xl md:leading-tight lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px]'>
-              I Build & Design <br /> Web Interfaces.
-            </h1>
-            <p className='pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] text-lg text-center lg:text-left'>
-              Lorem ipsum dolor sit amet consectetur adipisicing illo ad labore
-              dolor elit.
-            </p>
-            <button className='btn btn-md bg-accent hover:bg-secondary-hover md:btn-lg transition-all'>
-              Work with me
-            </button>
-          </div>
-          <div className='hidden lg:flex flex-1 justify-end items-end h-full'>
-            <img src={WomanImg} alt='' />
-          </div>
-        </div>
+    <section 
+    className='lg: h-[85vh] flex items-center bg-primary lg: bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden'>
+      <div className='container mx-auto'>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
